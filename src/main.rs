ca1ad6b62651ad65b29f50155e1c591d07cf9f0b
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-05-19 11:23:04 +0800

    Replace option

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
+extern crate clap;
+
+use clap::{Arg, App, Values};
 use std::{fs, io};
 use std::fs::DirEntry;
 use std::path::{Path, PathBuf};
+use std::collections::HashMap;
 
 fn main() {
-    let path = std::env::args()
-        .nth(1)
-        .expect("Path input required.");
+    let matches = App::new("resname")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("PATH")
+                .help("The path of file to rename.")
+                .required(true)
+                .index(1)
+        )
+        .arg(
+            Arg::with_name("replace")
+                .short("r")
+                .long("replace")
+                .value_name("string")
+                .help("For replace matched strings to _.")
+                .takes_value(true)
+                .multiple(true)
+                .required(false)
+        )
+        .get_matches();
+    let path = matches.value_of("PATH").unwrap().to_string();
+
     visit_dirs(Path::new(path.as_str()), &|file: &DirEntry| {
         let i_file_name = file.file_name();
         let mut buf = PathBuf::new();
         buf.push(file.path().parent().unwrap().to_str().unwrap());
-        let x = i_file_name.to_str().unwrap().chars();
-        let s: String = x.map(|x| match x {
-            ' ' | '-' => '_',
-            'A'..='Z' => x.to_ascii_lowercase(),
-            _ => x
+        let file_name = i_file_name.to_str().unwrap().chars();
+        let updated_name: String = file_name.map(|char| {
+            let replace: Vec<&str> = matches.values_of("replace").unwrap_or(Values::default()).collect();
+            match char {
+                ' ' | '-' => '_',
+                'A'..='Z' => char.to_ascii_lowercase(),
+                _ => if replace.contains(&char.to_string().as_str()) {
+                    '_'
+                } else {
+                    char
+                }
+            }
         }).collect();
-        buf.push(s);
+        buf.push(updated_name);
         let origin_path = file.path();
         let origin_path = origin_path.to_str().unwrap();
         let target_path = buf.to_str().unwrap();
